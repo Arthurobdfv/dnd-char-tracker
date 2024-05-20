f820d2ea9797aa3ff4315a4d0b20d827f67348a5
Nicholas Garrett <nicholasgarrett98@icloud.com>
2024-05-20 18:17:05 +0100

    Hp tracker css js and index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+/* HP config*/
+let yourHealth = 100;
+setInterval(function(){
+    let healthvariable = document.getElementById("health");
+    healthvariable.value = yourHealth;   
+}, 1);
 
+
+function addHP(){
+    yourHealth += 10;
+}
+
+function loseHp(){
+  yourHealth -= 1;
+}
