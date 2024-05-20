d99f9d98a4ca80a1d3a89e3cb46ed5b072fef1f2
Arthuro Veríssimo <arthurobdfv@gmail.com>
2024-05-20 15:45:03 +0100

    Adding Actionsurge on index.js
    Added logic for the ActionSurge in the Index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+var longRestBtn = document.getElementsByClassName('long-rest-event-button')[0];
+var count = 0;
+var actionSurgeComponent = document.getElementsByClassName();
+longRestBtn.addEventListener("click",(e) => {
+  console.log("Clicou em ShortRest");
+})
