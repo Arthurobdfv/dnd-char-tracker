64fb9fedf2b2d29f74e1dff430adedf642b68c45
Arthuro Veríssimo <arthurobdfv@gmail.com>
2024-05-20 15:51:26 +0100

    Updating ActionSurge index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 var longRestBtn = document.getElementsByClassName('long-rest-event-button')[0];
 var count = 0;
-var actionSurgeComponent = document.getElementsByClassName();
+var actionSurgeComponent = document.getElementsByClassName("action-surge-counter-display");
+
 longRestBtn.addEventListener("click",(e) => {
+  count += 1;
   console.log("Clicou em ShortRest");
+  updateComponent();
 })
+
+
+function updateComponent(){
+  actionSurgeComponent.innerHTML = count + "/1";
+}
