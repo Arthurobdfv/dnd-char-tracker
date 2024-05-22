108b4e391a2eac09910823b50baf04c49ca36c40
Arthuro Veríssimo <arthurobdfv@gmail.com>
2024-05-22 10:08:12 +0100

    Added a new folder structure to support different components and different folders.

---
+++ components/action-surge/actionSurge.js
@@ -0,0 +1,20 @@
+var actionSurgeComponent = document.getElementById(`action-surge-tracker`);
+var actionSurgeComponentText = document.getElementById(`action-surge-tracker-text`);
+var actionSurgeComponentDisplay = document.getElementById(`action-surge-counter-display`);
+
+var longRestButton = document.getElementById('long-rest-event-button');
+var count = 0;
+var maxCharges = 1;
+
+longRestButton.addEventListener("click", (event) => {
+    count ++;
+    if(count > maxCharges){
+        count = maxCharges;
+    };
+    updateUI();
+});
+updateUI();
+
+function updateUI(){
+    actionSurgeComponentDisplay.innerHTML = `${count}/${maxCharges}`;
+}
