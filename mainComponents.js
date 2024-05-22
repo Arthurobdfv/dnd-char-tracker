53f41519c62c0e0ae06959efa561c85fb189ea06
Arthuro Veríssimo <arthurobdfv@gmail.com>
2024-05-22 10:39:37 +0100

    Adding small comment

--- mainComponents.js
+++ mainComponents.js
@@ -1,2 +1,5 @@
+// This script loads longRestButton and shortRestButton to be globally
+// accessible throughout all the other components, see 
+//components/actionSurge/actionSurge.js as an example
 var longRestButton = document.getElementById(`long-rest-event-button`);
 var shortRestButton = document.getElementById(`short-rest-event-button`);
